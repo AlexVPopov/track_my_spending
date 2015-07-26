e233227937082eb5d76f51bbac90b3f4f9e78a5b
Alexander Popov <alex.val.popov@gmail.com>
2015-07-26 20:46:28 +0000

    Add model validations

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,7 @@
 class Expense < ActiveRecord::Base
   belongs_to :user
 
-  monetize :amount_stotinkas, numericality: {greater_than_or_equal_to: 0}
+  monetize :amount_stotinkas, numericality: {greater_than_or_equal_to: 0.1}
+
+  validates :date, presence: true
 end
