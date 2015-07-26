29971cda365745aa0f854751a9555b5d04ed5a2c
Alexander Popov <alex.val.popov@gmail.com>
2015-07-26 19:34:54 +0000

    Add amount column to Expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,5 @@
 class Expense < ActiveRecord::Base
   belongs_to :user
+
+  monetize :amount_stotinkas, numericality: {greater_than_or_equal_to: 0}
 end
---
+++ db/migrate/20150726184019_change_amount_column_of_expense.rb
@@ -0,0 +1,19 @@
+class ChangeAmountColumnOfExpense < ActiveRecord::Migration
+  def change
+    rename_column :expenses, :amount, :amount_old
+    add_monetize :expenses, :amount
+    reversible do |dir|
+      dir.up do
+        Expense.find_each { |expense| expense.update(amount: expense.amount_old) }
+        remove_column :expenses, :amount_old
+      end
+      dir.down do
+        add_column :expenses, :amount_old, :integer
+        Expense.find_each { |expense| expense.update(amount_old: expense.amount) }
+      end
+    end
+    
+    
+    
+  end
+end
