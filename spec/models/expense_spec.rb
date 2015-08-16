3587857d5911106a9c46c7402d7b40a7b3349a25
Alexander Popov <alex.val.popov@gmail.com>
2015-08-16 15:47:41 +0300

    Add Expense model specs

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it('has a valid fabricator') { expect(Fabricate(:expense)).to be_valid }
+
+  context 'validations' do
+    it { should validate_presence_of(:date) }
+    it { should validate_numericality_of(:amount).is_greater_than_or_equal_to(0.1) }
+  end
+
+  context 'associations' do
+    it { should belong_to(:user) }
+  end
+
+  context 'money' do
+    it { is_expected.to monetize(:amount).with_currency(:bgn) }
+  end
 end
