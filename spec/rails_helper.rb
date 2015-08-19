c986dc95defed93ab568773c2ec7b513953f9d93
Alexander Popov <alex.val.popov@gmail.com>
2015-08-19 14:24:12 +0300

    Prepare test environment for feature specs
    * Add capybara and database cleaner gems
    
    * Require capybara and database cleaner in rails helper
    
    * Configure database cleaner
    
    * Add selenium-webdriver gem
    
    * Add poltergeist gem
    
    * Require poltergeist in rails helper
    
    * Require wardon test helpers in rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,34 +9,30 @@ require 'support/controller_macros'
 require 'devise'
 require 'shoulda/matchers'
 require 'money-rails/test_helpers'
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+require 'database_cleaner'
 
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
   config.include Devise::TestHelpers, type: :controller
   config.extend ControllerMacros, :type => :controller
+  config.include Warden::Test::Helpers
+  config.before(:suite) { Warden.test_mode! }
+  config.after(:each) { Warden.test_reset! }
+
+  config.before(:suite) { DatabaseCleaner.clean_with(:truncation) }
+  config.before(:each) do |example|
+    DatabaseCleaner.strategy= example.metadata[:js] ? :truncation : :transaction
+    DatabaseCleaner.start
+  end
+  config.after(:each) { DatabaseCleaner.clean }
 end
 
 Shoulda::Matchers.configure do |config|
@@ -45,3 +41,5 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+Capybara.javascript_driver = :poltergeist
