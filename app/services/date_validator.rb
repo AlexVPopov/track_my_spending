a4ad0f60707c6816bd7630051b9f4b4a247b0567
Alexander Popov <alex.val.popov@gmail.com>
2015-10-19 12:54:52 +0300

    Refactor DateValidator

--- app/services/date_validator.rb
+++ app/services/date_validator.rb
@@ -4,23 +4,24 @@ class DateValidator
   def check_dates
     ensure_start_before_end_date
     ensure_start_date_valid
-    ensure_end_date_valid
   end
 
   private
 
+    class InvalidStartDate < StandardError; end
+    class EndDateBeforeStartDate < StandardError; end
+
     def ensure_start_before_end_date
-      raise ArgumentError, 'Start date must be before end date.' if start_date > end_date
+      return unless end_date
+      if start_date > end_date
+        raise EndDateBeforeStartDate, 'Start date must be before end date.'
+      end
     end
 
     def ensure_start_date_valid
       oldest_expense_date = Expense.oldest_date
       if start_date < oldest_expense_date
-        raise ArgumentError, "Start date must be after #{oldest_expense_date}."
+        raise InvalidStartDate, "Start date must be after #{oldest_expense_date}."
       end
-    end
-
-    def ensure_end_date_valid
-      raise ArgumentError, 'End date must be before today.' if end_date > Time.zone.today
     end
 end
