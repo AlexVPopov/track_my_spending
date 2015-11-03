5b144cec8ba35613f018c13aac274848685da6b6
Alexander Popov <alex.val.popov@gmail.com>
2015-11-03 16:08:32 +0200

    Remove wrongly set default date of expense on the db level

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007102957) do
+ActiveRecord::Schema.define(version: 20151103140217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "expenses", force: :cascade do |t|
     t.integer  "user_id"
-    t.datetime "created_at",                              null: false
-    t.datetime "updated_at",                              null: false
-    t.date     "date",             default: '2015-07-26', null: false
-    t.integer  "amount_stotinkas", default: 0,            null: false
-    t.string   "amount_currency",  default: "BGN",        null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.date     "date",                             null: false
+    t.integer  "amount_stotinkas", default: 0,     null: false
+    t.string   "amount_currency",  default: "BGN", null: false
   end
 
   add_index "expenses", ["user_id"], name: "index_expenses_on_user_id", using: :btree
