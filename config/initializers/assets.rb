0035cb8e6db0a053c90ddd3d6e02fce9a4ad6b00
Alexander Popov <alex.val.popov@gmail.com>
2015-11-05 13:56:57 +0200

    Set debugging of assets to false

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( select2.css select2.js )
-Rails.application.config.assets.debug = true
+Rails.application.config.assets.debug = false
