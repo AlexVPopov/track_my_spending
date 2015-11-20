d8b2257a545417b166e9bef0a28d4592db8370c0
Alexander Popov <alex.val.popov@gmail.com>
2015-11-20 12:51:35 +0200

    Remove assets from precompilation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,10 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( bootstrap-social.css
-                                                  datatables.js
-                                                  datatables.css
-                                                  select2.css
-                                                  select2.js
-                                                  select2-bootstrap.css )
+Rails.application.config.assets.precompile += %w()
 Rails.application.config.assets.debug = false
