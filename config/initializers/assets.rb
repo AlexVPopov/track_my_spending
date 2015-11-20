b449530707f25b09853c1b66c5fc634e3f2a7c16
Alexander Popov <alex.val.popov@gmail.com>
2015-11-20 09:15:41 +0200

    Add datatables js and css and add for precompilation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,8 +9,10 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( select2.css
+Rails.application.config.assets.precompile += %w( bootstrap-social.css
+                                                  datatables.js
+                                                  datatables.css
+                                                  select2.css
                                                   select2.js
-                                                  select2-bootstrap.css
-                                                  bootstrap-social.css )
+                                                  select2-bootstrap.css )
 Rails.application.config.assets.debug = false
