48f7202c6e1c7a06f4da2e0757746cf43856aac0
Alexander Popov <alex.val.popov@gmail.com>
2015-11-20 09:12:37 +0200

    Remove datatables related files and add files for precompilation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( select2.css select2.js )
+Rails.application.config.assets.precompile += %w( select2.css
+                                                  select2.js
+                                                  select2-bootstrap.css
+                                                  bootstrap-social.css )
 Rails.application.config.assets.debug = false
