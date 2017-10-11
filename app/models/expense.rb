4d5a2ddcfded0f0a31e1ed88976117efcc5115f4
Alexander Popov <alex.val.popov@gmail.com>
2017-10-11 13:06:25 +0300

    Update money-rails

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,7 @@
 class Expense < ActiveRecord::Base
   belongs_to :user
 
-  monetize :amount_stotinkas, numericality: {greater_than_or_equal_to: 0.1}
+  monetize :amount_stotinkas, numericality: {greater_than_or_equal_to: 0.1}, as: 'amount'
 
   validates :date, presence: true
   validates :user, presence: true
