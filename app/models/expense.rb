4bca4e0524d80a642989224c8d358a0a6da9e374
Alexander Popov <alex.val.popov@gmail.com>
2017-11-05 20:19:39 +0200

    Code style refactoring

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class Expense < ApplicationRecord
-  belongs_to :user
-
   monetize :amount_stotinkas, numericality: { greater_than_or_equal_to: 0.1 }, as: 'amount'
+
+  acts_as_ordered_taggable
+
+  belongs_to :user
 
   validates :date, presence: true
   validates :user, presence: true
-
-  acts_as_ordered_taggable
 
   after_initialize { |expense| expense.date ||= Time.zone.today }
 
