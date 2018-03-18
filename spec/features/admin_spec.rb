207ab61de12143dd40aba20ef4bbc8a0f98e64b7
Alexander Popov <alex.val.popov@gmail.com>
2018-03-18 17:09:41 +0200

    Show admins link to the admin panel

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -8,6 +8,12 @@ feature 'Access to admin dashboard' do
 
     background { login_as(user, scope: :user) }
 
+    scenario "they don't see a link to the admin dashboard" do
+      visit authenticated_root_path
+
+      expect(page).not_to have_content('Admin panel')
+    end
+
     scenario 'they get redirected to the root page' do
       visit admin_root_path
 
@@ -21,7 +27,9 @@ feature 'Access to admin dashboard' do
     background { login_as(user, scope: :user) }
 
     scenario 'they are given access to the admin dashboard' do
-      visit admin_root_path
+      visit authenticated_root_path
+
+      click_link 'Admin panel'
 
       expect(page.current_path).to eq admin_root_path
     end
